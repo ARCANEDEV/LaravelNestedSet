1b47f8418ce41f2f2ddeec5002b716ce20f73267
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-02-23 15:01:27 +0000

    Adding NestedSet Utility

---
+++ src/Utilities/NestedSet.php
@@ -0,0 +1,103 @@
+<?php namespace Arcanedev\LaravelNestedSet\Utilities;
+
+use Arcanedev\LaravelNestedSet\Traits\NodeTrait;
+use Illuminate\Database\Schema\Blueprint;
+
+/**
+ * Class     NestedSet
+ *
+ * @package  Arcanedev\Taxonomies\Utilities
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class NestedSet
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Constants
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The name of default lft column.
+     */
+    const LFT = '_lft';
+
+    /**
+     * The name of default rgt column.
+     */
+    const RGT = '_rgt';
+
+    /**
+     * The name of default parent id column.
+     */
+    const PARENT_ID = 'parent_id';
+
+    /**
+     * Insert direction.
+     */
+    const BEFORE = 1;
+
+    /**
+     * Insert direction.
+     */
+    const AFTER = 2;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get a list of default columns.
+     *
+     * @return array
+     */
+    public static function getDefaultColumns()
+    {
+        return [self::LFT, self::RGT, self::PARENT_ID];
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Migration Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Add default nested set columns to the table. Also create an index.
+     *
+     * @param  \Illuminate\Database\Schema\Blueprint  $table
+     */
+    public static function columns(Blueprint $table)
+    {
+        $table->unsignedInteger(self::LFT);
+        $table->unsignedInteger(self::RGT);
+        $table->unsignedInteger(self::PARENT_ID)->nullable();
+
+        $table->index(self::getDefaultColumns());
+    }
+
+    /**
+     * Drop NestedSet columns.
+     *
+     * @param  \Illuminate\Database\Schema\Blueprint  $table
+     */
+    public static function dropColumns(Blueprint $table)
+    {
+        $columns = self::getDefaultColumns();
+
+        $table->dropIndex($columns);
+        $table->dropColumn($columns);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Check Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Replaces instanceof calls for this trait.
+     *
+     * @param  mixed  $node
+     *
+     * @return bool
+     */
+    public static function isNode($node)
+    {
+        return is_object($node) && in_array(NodeTrait::class, (array) $node);
+    }
+}
